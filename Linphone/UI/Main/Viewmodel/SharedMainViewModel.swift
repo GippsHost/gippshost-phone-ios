9fc7b626b8ea76ff747036387acf1e91308bb855
Aaron Patton <aaron@gippshost.com.au>
2026-08-25 15:05:54 +1000

    Open dialler by default

--- Linphone/UI/Main/Viewmodel/SharedMainViewModel.swift
+++ Linphone/UI/Main/Viewmodel/SharedMainViewModel.swift
@@ -35,7 +35,7 @@ class SharedMainViewModel: ObservableObject {
 	@Published var generalTermsAccepted = false
 	@Published var displayProfileMode = false
 	@Published var defaultAvatar: URL?
-	@Published var indexView: Int = 0
+	@Published var indexView: Int = 1
 	@Published var increaseTrustLevelPopupAccepted = false
 	@Published var increaseTrustLevelPopupDeviceName = ""
 	@Published var increaseTrustLevelPopupDeviceAddress: Address?
@@ -77,11 +77,10 @@ class SharedMainViewModel: ObservableObject {
 	private init() {
 		let preferences = UserDefaults.standard
 		
-		if preferences.object(forKey: indexViewKey) == nil {
-			preferences.set(indexView, forKey: indexViewKey)
-		} else {
-			indexView = preferences.integer(forKey: indexViewKey)
-		}
+		// GippsHost Phone is calling-first: always start on Calls, while still
+		// allowing the customer to select Contacts from the bottom navigation.
+		indexView = 1
+		preferences.set(indexView, forKey: indexViewKey)
 		
 		if preferences.object(forKey: welcomeViewKey) == nil {
 			preferences.set(welcomeViewDisplayed, forKey: welcomeViewKey)
